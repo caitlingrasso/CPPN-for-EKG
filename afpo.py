35909868f5717e158ba1678e2d0028f90d755b75
Caitlin Grasso <caitlingrasso@caitlins-mbp-2.lan>
2020-06-17 14:45:29 -0400

    outline

--- afpo.py
+++ afpo.py
@@ -4,4 +4,18 @@
 class AFPO:
 
     def __init__(self):
+        # next_available_ID = 0
+        # create initial population
+        pass
+
+
+    def run(self):
+        # evaluate first generation
+
+        # for g in gens:
+            # expand population
+            # increase age
+            # add random individual
+            # evaluate population
+            # reduce population to target size
         pass
